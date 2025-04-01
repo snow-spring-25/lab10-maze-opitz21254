ae2d04ccbf69818e9ae01862fb661840ce7e6422
Brennan Opitz <brennan.opitz@students.snow.edu>
2025-04-01 08:30:14 -0600

    Create Mirmaid Syntax

--- MazeWeb/Program.cs
+++ MazeWeb/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddMermaidJS(); //<< -- Add this line
 
 var app = builder.Build();
 
